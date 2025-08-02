080ea78516e901e7b4d2cecca52ddd1549aca39b
Syed Wyle Mustafa <wylemustafa@gmail.com>
2025-08-02 16:25:58 +0500

    zz

--- api/get-slots.js
+++ api/get-slots.js
@@ -12,6 +12,8 @@ export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Methods", "POST,OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
+  res.setHeader("Allow", ["POST", "OPTIONS"]);
+
   if (req.method === "OPTIONS") {
     return res.status(200).end();
   }
